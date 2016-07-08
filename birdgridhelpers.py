8ca0701da4c1067c56190e6e5928c295ccec761e
SwarnaVallurupalli <swarna.vallurupalli@gmail.com>
2016-07-08 01:48:45 -0400

    init_birdgrid function takes the Observations DataFrame and divides the data into grids
    Each grid represents the count of  that particular species found in that
    grid, over a particular month starting from start_year to end_year.

--- birdgridhelpers.py
+++ birdgridhelpers.py
@@ -4,25 +4,57 @@ import glob
 
 #Takes in a set of desired attributes, the species, and the year range
 #returns an observation x [lat, lon, season, attribute_1, attribute_2,...attribute_n] matrix 
-def load_observations(attributes,species, start_year, end_year):
-path =r'C:\Users\swarna\Desktop\DENSITY\Grid\glob' 
+def load_observations(attributes,species,start_year,end_year):
+	path =r'C:\Users\swarna\Desktop\DENSITY\Grid\glob' 
 	allFiles = glob.glob(path + "/*.csv")
 	observations = pd.DataFrame()
-	#attributes=['LATITUDE','LONGITUDE']
-	#species=['Turdus_migratorius']
+	
 	ColumnNames=np.append(attributes,species)
 	list_ = []
 	for file_ in allFiles:
 		df = pd.read_csv(file_,index_col=None,header=0,usecols=ColumnNames)
 		list_.append(df)
-	 observations= pd.concat(list_)
+	observations= pd.concat(list_)
+	observations=observations[(observations['YEAR']>=start_year)&(observations['YEAR']<=end_year)]
 	return observations
-	#return np.array([])
-
+'''
+attributes=['LATITUDE','LONGITUDE','YEAR','MONTH']
+species=['Turdus_migratorius']
+start_year=2002
+end_year=2004
+observations=load_observations(attributes,species,start_year,end_year)
+grid_size=1
+'''
 #Takes in the matrix of observations and bins it into observations based on the provided grid size.
 #Returns an array of dicts, each dict represents one location and contains lat, lon and data for each timestep
-def init_birdgrid(observations, grid_size):
-	return []
+#Returns a dataframe of attributes that are divided into grids, where as each grid square represents the total count of the species found in that location
+def init_birdgrid(observations,grid_size,species):
+	#headers=observations.columns.values
+	observations=observations.replace('X',1) 
+	observations=observations.replace('?',0)
+	lats=observations['LATITUDE']
+	lons=observations['LONGITUDE']
+	observations=observations.convert_objects(convert_numeric=True)
+	lat_min = int(math.floor(min(lats)))
+	lat_max = int(math.floor(max(lats)))
+	lon_min =int(math.floor(min(lons)))
+	lon_max =int(math.floor(max(lons)))
+	GridSquare=[]
+	df=pd.DataFrame([])
+	
+	#starting the grid value with the minimum latitude and longitude values and iterating through the size of the grid 
+
+	for i in range(lat_min,lat_max,grid_size):
+		for j in range(lon_min,lon_max,grid_size):
+			GridSquare=observations[(observations['LATITUDE']>=i)&(observations['LATITUDE']<i+grid_size)&(observations['LONGITUDE']>=j)&(observations['LONGITUDE']<j+grid_size)]
+			GridSquare['LATITUDE']=i #replacing the lat value with the starting value of that particular grid
+			GridSquare['LONGITUDE']=j #replacing the lon value with that of the starting value of that particular grid
+			#replaced lat,lon in order to group the species count
+			GridwiseCount=GridSquare.groupby(['LATITUDE','LONGITUDE','YEAR','MONTH'],as_index=False)[species].sum() 
+			df=df.append(GridwiseCount)
+	return df
+Grid_Data=init_birdgrid(observations,grid_size,species)
+	
 
 #Plot the actual species frequency (from the data) on a map
 def plot_observation_frequency(observations):
