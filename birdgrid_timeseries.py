8596dda0a659c1e4819779507b1534fb04b8767d
SwarnaVallurupalli <swarna.vallurupalli@gmail.com>
2016-07-28 00:06:54 -0400

    Updating the functions attributes

--- birdgrid_timeseries.py
+++ birdgrid_timeseries.py
@@ -1,26 +1,41 @@
 import numpy as np
-from geopy.distance import vincenty
-from birdgridhelpers import load_observations,init_birdgrid,plot_observation_frequency,plot_predictor_locations
+from birdgridhelpers import load_observations,init_birdgrid,plot_observation_frequency,model_location_novelty_over_time
+import numpy as np
+import glob
+import math
+import pandas as pd
+from scipy import interpolate
+from datetime import datetime
+from mpl_toolkits.basemap import Basemap
+from matplotlib.mlab import griddata
+from scipy import interpolate
+import csv 
+import matplotlib.pyplot as plt
+from sklearn import linear_model
 
-SPECIES = ""
-ATTRIBUTES = []
+SPECIES = ['']
+ATTRIBUTES = ['LATITUDE','LONGITUDE','YEAR','MONTH']
 START_YEAR = 2002
-END_YEAR = 2016
+END_YEAR = 2012
+TIME_STEP = "monthly" 
 GRID_SIZE = 1 #Side length of each grid square (in degrees lat/lon)
-
+predictors = []
+SEASONS = {"winter": [12,1,2],"spring": [3,4,5],"summer":[6,7,8],"fall":[9,10,11]}
+YEARS=[2002,2003,2004,2005,2006,2007,2008,2009,2010,2011]
 observations = load_observations(ATTRIBUTES, SPECIES, START_YEAR, END_YEAR) #Load these in from somewhere, one row per observation, columns 0 and 1 are lat and lon
-locations = init_birdgrid(observations, GRID_SIZE, TIME_STEP) #Calculate these from the above, Array of dicts, each dict contains lat, lon and data for each timestep
+locations=init_birdgrid(observations,GRID_SIZE,SPECIES,TIME_STEP)  #Calculate these from the above, Array of dicts, each dict contains lat, lon and data for each timestep
 
 #Plot our species frequency observations
-plot_observation_frequency(locations)
+plot_observation_frequency(locations,YEARS,SEASONS,GRID_SIZE)
 
 #For each location (grid square), plot the birdcount over time. Additionally display the location within the US on a small inset mapbox
 for loc in locations:
 	plot_birds_over_time(loc)
 	
-predictors = [] # matrix of models of shape locations x timesteps. 
-for loc in locations:
-	predictors.append(model_location_novelty_over_time(loc))
+# matrix of models of shape locations x timesteps. 
+for k,location in locations.groupby(['LATITUDE','LONGITUDE'],as_index=False):
+	predictors.append(model_location_novelty_over_time(location,SPECIES,YEARS,SEASONS))
+
 
 #Each location will have an array of predictors associated with it -- one per timestep.
 for preds,loc in zip(predictors,locations):
